ac3974052fd9885dd99068a61a2000c951f8a342
philipmorrone <philip.j.morrone@gmail.com>
2021-12-03 21:25:06 -0500

    fuck git

--- eval.py
+++ eval.py
@@ -1,19 +1,6 @@
-def evaluator(lolists):
-    for ins in lolists:
-        if ins[0] == "add":
-            print("it's add")
+def evaluator(ins):
+    print(ins[1])
 
-
-        elif ins[0] == "sub":
-            print("it's sub")
-
-        elif ins[0] == "lw":
-            print("it's lw")
-
-        elif ins[0] == "sw":
-            print("it's sw")
-
-
-#add (arg 1), sub (arg 1), lw (arg1) can start a dependency;
-#and add (args 2 and 3), sub (args 2 and 3), lw (arg 3), and sw (arg 1 and arg 3) can be linked to it.
-#for lw and sw, I am skipping arg 2 which is a constant offset.
+# add (arg 1), sub (arg 1), lw (arg1) can start a dependency;
+# and add (args 2 and 3), sub (args 2 and 3), lw (arg 3), and sw (arg 1 and arg 3) can be linked to it.
+# for lw and sw, I am skipping arg 2 which is a constant offset.
--- main.py
+++ main.py
@@ -3,19 +3,32 @@ import eval
 
 file1 = open("inputProg.txt", "r")
 inputProg = file1.readlines()
-instructions = []  #list of instructions
+instructions = []  # list of instructions
 
 for i in range(len(inputProg)):
     instructions.append(inputProg[i].strip('\n'))
     print(instructions[i])
 
-myLists = listoflistins.lol(instructions)
+# Tokenized Instructions
+listOfIns = listoflistins.lol(instructions)
 
-for i in myLists:
+for i in listOfIns:
     print(i)
 
-
-eval.evaluator(myLists)
-
-print()
-
+for x in range(0, 4):
+    if x < 2:
+        next2 = listOfIns[x+1]+listOfIns[x+2]
+        #print("next 2:", next2)
+        arg1 = listOfIns[x][1]
+        if listOfIns[x][0]is not "sw":
+            if arg1 in next2:
+                haz = listOfIns[x].index(arg1)
+                print("Data Dependency:", listOfIns[x], listOfIns[x+1], "at index: ", haz)
+    elif x==2 :
+        # print("next 1:", listOfIns[x+1])
+        print()
+    elif x==3 :
+        # print("don't need to check")
+        print()
+    else:
+        print("fallthrough")
