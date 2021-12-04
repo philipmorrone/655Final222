37088c996dad06c8282d466e34844cfe2e5784e2
philipmorrone <philip.j.morrone@gmail.com>
2021-12-04 17:33:08 -0500

    fuck git

---
+++ createTD.py
@@ -0,0 +1,22 @@
+def newTD(void):
+    ins1 = ["F", "D", "X", "M", "W"]
+    ins2 = ["-", "F", "D", "X", "M", "W"]
+    ins3 = ["-", "-", "F", "D", "X", "M", "W"]
+    ins4 = ["-", "-", "-", "F", "D", "X", "M", "W"]
+
+    initTD = []
+
+    initTD.append(ins1)
+    initTD.append(ins2)
+    initTD.append(ins3)
+    initTD.append(ins4)
+
+    # ins2.insert(2,"s")
+    # ins3.insert(2,"-")
+    # ins4.insert(3,"-")
+
+    # print(initTD, "\n")
+
+    # print("", ins1, "\n", ins2, "\n", ins3, "\n", ins4)
+
+    return initTD
