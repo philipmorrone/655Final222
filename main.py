ee2af232fbf12e8ef77d1dd75a0a9c74a2d4c8ef
philipmorrone <philip.j.morrone@gmail.com>
2021-12-04 16:59:19 -0500

    fuck git

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import listoflistins
-import eval
+import depdetector
 
 file1 = open("inputProg.txt", "r")
 inputProg = file1.readlines()
@@ -15,38 +15,5 @@ ListOfIns = listoflistins.lol(instructions)
 for i in ListOfIns:
     print(i)
 
+depdetector.detector(ListOfIns)
 
-
-for x in range(0, 4):
-    if x < 2:
-        next2 = myListOfIns[x + 1] + myListOfIns[x + 2]
-        # print(listOfIns[x][0])
-        # print("next 2:", next2)
-        if myListOfIns[x][0] != "sw":
-            arg1 = myListOfIns[x][1]
-            # print(arg1)
-            if arg1 in next2:
-                haz = myListOfIns[x].index(arg1)
-                if arg1 in myListOfIns[x + 1] and arg1 in myListOfIns[x + 2]:
-                    print("Data Dependency:", myListOfIns[x], myListOfIns[x + 1], "with register: ", myListOfIns[x][haz])
-                    print("Data Dependency:", myListOfIns[x], myListOfIns[x + 2], "with register: ", myListOfIns[x][haz])
-                elif arg1 in myListOfIns[x + 1]:
-                    print("Data Dependency:", myListOfIns[x], myListOfIns[x + 1], "with register: ", myListOfIns[x][haz])
-                elif arg1 in myListOfIns[x + 2]:
-                    print("Data Dependency:", myListOfIns[x], myListOfIns[x + 2], "with register: ", myListOfIns[x][haz])
-                else:
-                    continue
-    elif x == 2:
-        if myListOfIns[x][0] != "sw":
-            arg1 = myListOfIns[x][1]
-            # print(arg1)
-            if arg1 in myListOfIns[x + 1]:
-                haz = myListOfIns[x].index(arg1)
-                print("Data Dependency:", myListOfIns[x], myListOfIns[x + 1], "with register: ", myListOfIns[x][haz])
-        else:
-            continue
-    elif x==3 :
-        # print("don't need to check")
-        print()
-    else:
-        print("fallthrough")
