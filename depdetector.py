827447e775994ea17981847c8b0436360365e444
philipmorrone <philip.j.morrone@gmail.com>
2021-12-04 16:54:25 -0500

    fuck git

---
+++ depdetector.py
@@ -0,0 +1,39 @@
+def detector(myListOfIns):
+    for x in range(0, 4):
+        if x < 2:
+            next2 = myListOfIns[x + 1] + myListOfIns[x + 2]
+            # print(listOfIns[x][0])
+            # print("next 2:", next2)
+            if myListOfIns[x][0] != "sw":
+                arg1 = myListOfIns[x][1]
+                # print(arg1)
+                if arg1 in next2:
+                    haz = myListOfIns[x].index(arg1)
+                    if arg1 in myListOfIns[x + 1] and arg1 in myListOfIns[x + 2]:
+                        print("Data Dependency:", myListOfIns[x], myListOfIns[x + 1], "with register: ",
+                              myListOfIns[x][haz])
+                        print("Data Dependency:", myListOfIns[x], myListOfIns[x + 2], "with register: ",
+                              myListOfIns[x][haz])
+                    elif arg1 in myListOfIns[x + 1]:
+                        print("Data Dependency:", myListOfIns[x], myListOfIns[x + 1], "with register: ",
+                              myListOfIns[x][haz])
+                    elif arg1 in myListOfIns[x + 2]:
+                        print("Data Dependency:", myListOfIns[x], myListOfIns[x + 2], "with register: ",
+                              myListOfIns[x][haz])
+                    else:
+                        continue
+        elif x == 2:
+            if myListOfIns[x][0] != "sw":
+                arg1 = myListOfIns[x][1]
+                # print(arg1)
+                if arg1 in myListOfIns[x + 1]:
+                    haz = myListOfIns[x].index(arg1)
+                    print("Data Dependency:", myListOfIns[x], myListOfIns[x + 1], "with register: ",
+                          myListOfIns[x][haz])
+            else:
+                continue
+        elif x == 3:
+            # print("don't need to check")
+            print()
+        else:
+            print("fallthrough")
--- main.py
+++ main.py
@@ -10,37 +10,39 @@ for i in range(len(inputProg)):
     #print(instructions[i])
 
 # Tokenized Instructions
-listOfIns = listoflistins.lol(instructions)
+ListOfIns = listoflistins.lol(instructions)
 
-for i in listOfIns:
+for i in ListOfIns:
     print(i)
+
+
 
 for x in range(0, 4):
     if x < 2:
-        next2 = listOfIns[x+1]+listOfIns[x+2]
+        next2 = myListOfIns[x + 1] + myListOfIns[x + 2]
         # print(listOfIns[x][0])
         # print("next 2:", next2)
-        if listOfIns[x][0] != "sw":
-            arg1 = listOfIns[x][1]
+        if myListOfIns[x][0] != "sw":
+            arg1 = myListOfIns[x][1]
             # print(arg1)
             if arg1 in next2:
-                haz = listOfIns[x].index(arg1)
-                if arg1 in listOfIns[x+1] and arg1 in listOfIns[x+2]:
-                    print("Data Dependency:", listOfIns[x], listOfIns[x+1], "with register: ", listOfIns[x][haz])
-                    print("Data Dependency:", listOfIns[x], listOfIns[x+2], "with register: ", listOfIns[x][haz])
-                elif arg1 in listOfIns[x+1]:
-                    print("Data Dependency:", listOfIns[x], listOfIns[x + 1], "with register: ", listOfIns[x][haz])
-                elif arg1 in listOfIns[x+2]:
-                    print("Data Dependency:", listOfIns[x], listOfIns[x + 2], "with register: ", listOfIns[x][haz])
+                haz = myListOfIns[x].index(arg1)
+                if arg1 in myListOfIns[x + 1] and arg1 in myListOfIns[x + 2]:
+                    print("Data Dependency:", myListOfIns[x], myListOfIns[x + 1], "with register: ", myListOfIns[x][haz])
+                    print("Data Dependency:", myListOfIns[x], myListOfIns[x + 2], "with register: ", myListOfIns[x][haz])
+                elif arg1 in myListOfIns[x + 1]:
+                    print("Data Dependency:", myListOfIns[x], myListOfIns[x + 1], "with register: ", myListOfIns[x][haz])
+                elif arg1 in myListOfIns[x + 2]:
+                    print("Data Dependency:", myListOfIns[x], myListOfIns[x + 2], "with register: ", myListOfIns[x][haz])
                 else:
                     continue
     elif x == 2:
-        if listOfIns[x][0] != "sw":
-            arg1 = listOfIns[x][1]
+        if myListOfIns[x][0] != "sw":
+            arg1 = myListOfIns[x][1]
             # print(arg1)
-            if arg1 in listOfIns[x+1]:
-                haz = listOfIns[x].index(arg1)
-                print("Data Dependency:", listOfIns[x], listOfIns[x + 1], "with register: ", listOfIns[x][haz])
+            if arg1 in myListOfIns[x + 1]:
+                haz = myListOfIns[x].index(arg1)
+                print("Data Dependency:", myListOfIns[x], myListOfIns[x + 1], "with register: ", myListOfIns[x][haz])
         else:
             continue
     elif x==3 :
