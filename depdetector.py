8ede4369ab160e6052b4b1353a6fab3dad61ebff
philipmorrone <philip.j.morrone@gmail.com>
2021-12-14 23:51:13 -0500

    fuck git

--- depdetector.py
+++ depdetector.py
@@ -99,15 +99,19 @@ def nfu(myListOfIns):
                 myTD.insert(x + 1, 4, "s")
                 myTD.insert(x + 2, 0, "-")
                 myTD.insert(x + 2, 0, "-")
+
             elif arg1 in nextins2 and fired < 1 and myListOfIns[x][0] != "sw":
                 myTD.insert(x + 2, 4, "s")
+
             elif arg1 in nextIns and fired == 1 and myListOfIns[x][0] != "sw":
                 myTD.insert(x + 1, 5, "s")
                 myTD.insert(x + 1, 6, "s")
                 myTD.insert(x + 2, 0, "-")
                 myTD.insert(x + 2, 0, "-")
+
             elif arg1 in nextins2 and fired == 1 and myListOfIns[x][0] != "sw":
                 myTD.insert(x + 2, 5, "s")
+
             fired = 2
             print("Fired: ", fired)
 
@@ -115,7 +119,7 @@ def nfu(myListOfIns):
             nextIns = myListOfIns[x + 1]
             arg1 = myListOfIns[x][1]
             floc = myTD.ins4.index("F")
-            print("floc: ",floc)
+            # print("floc: ",floc)
 
             sw1 = 0
             if nextIns[0] == "lw" and arg1 in nextIns[1:4]:
